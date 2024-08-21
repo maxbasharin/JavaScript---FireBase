663e60d79412dbcf8b78ead8feabd60877e06f41
Max Basharin <maxbasharinweb@gmail.com>
2024-08-21 22:50:50 +0300

    сохранение записи firebase

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,6 +1,7 @@
 import { Component } from '../core/component'
 import { Form } from '../core/form'
 import { Validators } from '../core/validators'
+import { apiService } from '../services/api.service'
 
 export class CreateComponent extends Component {
   constructor(id) {
@@ -17,7 +18,7 @@ export class CreateComponent extends Component {
   }
 }
 
-function submitHandler(event) {
+async function submitHandler(event) {
   event.preventDefault()
 
   if (this.form.isValid()) {
@@ -26,9 +27,10 @@ function submitHandler(event) {
       date: new Date().toLocaleString(),
       ...this.form.value()
     }
-  
-    console.log('Submit', formData)
-  } else {
-    console.warn('Form is invalid')
-  }
+ 
+    await apiService.createPost(formData)
+    this.form.clear()
+    alert('Запись создана в базе данных')
+
+  } 
 }
---
+++ src/services/api.service.js
@@ -0,0 +1,20 @@
+class ApiService {
+  constructor(baseUrl) {
+    this.url = baseUrl
+  }
+
+  async createPost(post) {
+    try {
+      const request = new Request(this.url + '/posts.json', {
+        method: 'post',
+        body: JSON.stringify(post)
+      })
+      const response = await fetch(request)
+      return await response.json()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export const apiService = new ApiService('https://max-blog-js-default-rtdb.europe-west1.firebasedatabase.app/')
